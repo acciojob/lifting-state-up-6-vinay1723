c4b879f8df49ce7edff42ce3e40e34a0381dc994
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-08-13 10:37:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
+const tasks = [
+  { id: 1, description: "Learn React", complete: false },
+  { id: 2, description: "Build React App", complete: false },
+  { id: 3, description: "Deploy the React App", complete: false },
+];
 const App = () => {
+  const [todos, setTodos] = useState(tasks);
+
+  function handleComplete(id) {
+    setTodos((Prevtodos) =>
+      Prevtodos.map((todo) => {
+        return todo.id === id ? { ...todo, complete: true } : todo;
+      })
+    );
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>Parent Component</h1>
+      {/* Do not remove the main div */}
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
   )
 }

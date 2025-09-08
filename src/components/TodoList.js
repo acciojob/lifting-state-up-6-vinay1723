c5b909124ff0b4c00e19d9edbf36b7fa53df4234
vinay1723 <vinaykumar.17july@gmail.com>
2025-09-08 16:51:09 +0530

    Initial Commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,10 +8,17 @@ export default function TodoList(props) {
       <ul>
         {props.todos.map((task) => (
           <li key={task.id} style={{ display: "flex" }}>
-            <p style={{ margin: "5px 10px" }}>{task.description}</p>
+            <p
+              style={{
+                margin: "5px 10px",
+                textDecoration: task.completed ? "line-through" : "none",
+              }}
+            >
+              {task.description}
+            </p>
             {!task.complete && (
               <button onClick={(e) => props.handleComplete(task.id)}>
-                complete
+                Complete
               </button>
             )}
           </li>

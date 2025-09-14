290f1938d4a1d0a6e4eae690795544cf678f55dd
vinay1723 <vinaykumar.17july@gmail.com>
2025-09-14 11:42:10 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import "./../styles/App.css";
 import TodoList from "./TodoList";
 
-const tasks = [
-  { id: 1, description: "Learn React", complete: false },
-  { id: 2, description: "Build a React app", complete: false },
-  { id: 3, description: "Deploy the React app", complete: false },
-];
+// const tasks = [
+//   { id: 1, description: "Learn React", complete: false },
+//   { id: 2, description: "Build a React app", complete: false },
+//   { id: 3, description: "Deploy the React app", complete: false },
+// ];
+const tasks = [{ id: 1, description: "Learn React", complete: false }];
 const App = () => {
   const [todos, setTodos] = useState(tasks);
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -18,7 +18,7 @@ export default function TodoList(props) {
             </p>
             {!task.complete && (
               <button onClick={(e) => props.handleComplete(task.id)}>
-                Complete
+                complete
               </button>
             )}
           </li>
